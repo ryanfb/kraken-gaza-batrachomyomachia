d8921a59ba3181c2fbb3e2593bd29f9a1d531391
Ryan Baumann <ryan.baumann@gmail.com>
2020-06-09 16:47:10 -0400

    Expand ȣ ligatures

--- make-text.sh
+++ make-text.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-cat batrachomyomachia_*.txt | perl -p -e 's/-\n//' | sed -e 's/ϛ/στ/g' > gaza-batrachomyomachia.md
+cat batrachomyomachia_*.txt | perl -p -e 's/-\n//' | sed -e 's/ϛ/στ/g' -e 's/ȣ/ου/' | uconv -x any-nfc > gaza-batrachomyomachia.md
