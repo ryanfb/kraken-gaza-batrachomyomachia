87cbd60fae579d4ca5db32a7f1e5b3348592b002
Ryan Baumann <ryan.baumann@gmail.com>
2020-06-11 09:19:45 -0400

    Reorganize

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: gaza-batrachomyomachia.md gaza-batrachomyomachia-lineated.md
 
-gaza-batrachomyomachia.md: gaza-batrachomyomachia.yaml batrachomyomachia_*.txt make-text.sh
+gaza-batrachomyomachia.md: gaza-batrachomyomachia.yaml nfc/batrachomyomachia_prose_*.txt make-text.sh
 	./make-text.sh
 
 gaza-batrachomyomachia-lineated.md: gaza-batrachomyomachia.md lineate.rb line-endings.csv
--- make-text.sh
+++ make-text.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 
-cp gaza-batrachomyomachia.yaml gaza-batrachomyomachia.md && cat  batrachomyomachia_*.txt | perl -p -e 's/-\n//' | sed -e 's/ϛ/στ/g' -e 's/ȣ/ου/' | uconv -x any-nfc >> gaza-batrachomyomachia.md
+cp gaza-batrachomyomachia.yaml gaza-batrachomyomachia.md && \
+  cat nfc/batrachomyomachia_prose_*.txt | \
+  perl -p -e 's/-\n//' | \
+  sed -e 's/ϛ/στ/g' -e 's/ȣ/ου/' | \
+  uconv -x any-nfc >> gaza-batrachomyomachia.md
