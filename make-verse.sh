1a0cf8d3b3eb5e39c3baae38f17ba22982d8eccb
Ryan Baumann <ryan.baumann@gmail.com>
2020-06-11 09:47:31 -0400

    Don't try to run verse lines together

--- make-verse.sh
+++ make-verse.sh
@@ -2,6 +2,5 @@
 
 cp gaza-batrachomyomachia.yaml gaza-batrachomyomachia-verse.md && \
   cat nfc/batrachomyomachia_verse*.txt | \
-  perl -p -e 's/-\n//' | \
   sed -e 's/ϛ/στ/g' -e 's/ȣ/ου/' | \
   uconv -x any-nfc >> gaza-batrachomyomachia-verse.md
